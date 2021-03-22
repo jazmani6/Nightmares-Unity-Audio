4e0d5abbfb2415aba2509b503ba902333d6ab2b0
Jaz Mani <jaz_mani@yahoo.ca>
2021-03-22 15:39:27 -0400

    Initial Commit

---
+++ Assets/Scripts/Managers/BGMSwitchTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMSwitchTrigger : MonoBehaviour
+{
+    public WaveManager wave;
+    public AudioClip newBGM;
+    private BGMSelector BGM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMSelector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+    
---
+++ Assets/Scripts/Misc/BGMSelector.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMSelector : MonoBehaviour
+{
+    public AudioSource backgroundMusic;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchMusic (AudioClip music)
+    {
+        FadeOut(backgroundMusic, 2.0f);
+        backgroundMusic.clip = music;
+        FadeIn(backgroundMusic, 4.0f);
+
+    }
+
+    public void SwitchMusicOneShot(AudioClip music)
+    {
+        FadeOut(backgroundMusic, 2.0f);
+        backgroundMusic.clip = music;
+        backgroundMusic.PlayOneShot(music);
+
+    }
+
+    public void FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+           
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+
+    public void FadeIn(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = 0.2f;
+
+        audioSource.volume = 0;
+        audioSource.Play();
+
+        while (audioSource.volume < 1.0f)
+        {
+            audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+
+        }
+
+        audioSource.volume = 1f;
+    }
+}
